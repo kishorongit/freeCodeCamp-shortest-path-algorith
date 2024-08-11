56a1f2ca490626eb6574dec133311799f1471b7b
Kishor Subedi <subedi.kishor1987@gmail.com>
2024-08-11 13:27:49 +0545

    feat: add method to find shortest path

--- main.py
+++ main.py
@@ -4,3 +4,24 @@ my_graph = {
     'C': [('B', 4), ('D', 7)],
     'D': [('A', 1), ('C', 7)]
 }
+
+
+def shortest_path(graph, start):
+    unvisited = list(graph)
+    distances = {node: 0 if node == start else float('inf') for node in graph}
+    paths = {node: [] for node in graph}
+    paths[start].append(start)
+
+    while unvisited:
+        current = min(unvisited, key=distances.get)
+        for node, distance in graph[current]:
+            if distance + distances[current] < distances[node]:
+                distances[node] = distance + distances[current]
+                if paths[node] and paths[node][-1] == node:
+                    paths[node] = paths[current][:]
+                else:
+                    paths[node].extend(paths[current])
+                paths[node].append(node)
+        unvisited.remove(current)
+
+    print(f'Unvisited: {unvisited}\nDistances: {distances}\nPaths: {paths}')
